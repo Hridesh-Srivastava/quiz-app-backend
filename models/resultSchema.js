a6d88ff6b7be9a6bb51a2a1c283fb3fa4b293f71
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2025-03-15 22:35:29 +0530

    Refactor controller by removing commented-out code and update result schema to remove 'achived' field

--- models/resultSchema.js
+++ models/resultSchema.js
@@ -8,7 +8,6 @@ const resultModel = new Schema({
     result : { type : Array, default : []},
     attempts : { type : Number, default : 0},
     points : { type : Number, default : 0},
-    achived : { type : String, default : ''},
     createdAt : { type : Date, default : Date.now}
 })
 
