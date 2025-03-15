ae14d47cb8963d5b81bd9b82366024949aba323b
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2025-03-15 17:51:12 +0530

    Add constants and refactor database connection and controller functions

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 export default async function connect(){
-    await mongoose.connect(process.env.MONGODB_URI)
-    console.log(`MongoDB connected successfully! host: ${connectionInstance.connection.host}`);
+    try {
+        const connectionInstance = await mongoose.connect(`${ process.env.MONGODB_URI }/${ DB_NAME }`); //no space '/' k aage peeche
+            console.log(`MongoDB connected successfully! host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("Error while connecting to Database!" , error);
+        process.exit(1); //1 default value says overloaded nodejs process failure   
+    }
 }
